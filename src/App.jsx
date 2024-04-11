9381d8cb463a630bc8dc649ce5ff0924630a94f6
akshatajmera07 <akshatajmera777@gmail.com>
2024-04-12 00:35:24 +0530

    removed extra

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,6 @@ import { Home } from "./pages/Home";
 import styles from "./app.module.css";
 
 function App() {
-  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  // console.log(mousePosition);
-
-  // useEffect(() => {
-  //   const mouseMove = (e) => {
-  //     console.log(e);
-  //   };
-  //   window.addEventListener("mousemove", mouseMove);
-
-  //   return () => {
-  //     window.addEventListener("mousemove", mouseMove);
-  //   };
-  // }, []);
-  // const [count, setCount] = useState(0);
 
   return (
     <>
